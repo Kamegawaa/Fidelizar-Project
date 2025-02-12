450b51e547061a772ff3ca16db1879d3e082192c
Lucas Pires Kamegawa <126916359+Kamegawaa@users.noreply.github.com>
2025-02-12 15:03:57 -0300

    Update script.js

--- +LP/script.js
+++ +LP/script.js
@@ -1,1 +1,18 @@
-
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+  
+      if (targetSection) {
+        const offset = 80;
+        const targetPosition = targetSection.offsetTop - offset;
+  
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
